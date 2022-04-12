00871037bae7bced165144dd2c4cb319e9871145
Latif-Ibrahim <ibrahimabdulatif1@gmail.com>
2022-04-12 06:47:22 -0700

    added middlewares

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express= require("express")
 const usersRoute = require("./routes/usersRoutes")
 const path = require("path")
+const logger = require("./middlewares/user.logger")
+const booksRoute = require("./routes/booksRoute")
 
 const app = express()
 
 //middlewares
 app.use(express.json());
-
-
+app.use(logger)
 app.use(usersRoute)
+app.use(booksRoute)
 
 //home route
 app.get("/", (req, res)=>{
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,10 +1,11 @@
 const  {Router} = require("express")
+const auth = require("../middlewares/user.auth")
 
 const {getUsers, createUser, getUser} = require("../controllers/usersController")
 
 const router = Router();
-router.route("/users").get(getUsers).post(createUser).get(getUser)
-router.get("/users/:id", getUser)
+router.route("/users").get(auth, getUsers).post(createUser).get(getUser)
+router.get("/users/:id", auth, getUser)
 
 
 
